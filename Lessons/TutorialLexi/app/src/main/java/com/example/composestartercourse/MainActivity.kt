f14aa02b2aa510d1c27507413585933d61877936
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-02-03 21:09:28 +0000

    Added row with image and column with texts

--- Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
+++ Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composestartercourse.ui.theme.ComposeStarterCourseTheme
 
@@ -34,10 +37,17 @@ fun MessageCard(text: String)
 @Composable
 fun MessageCard(text1:String, text2:String)
 {
-    Column()
+    Row()
     {
-        Text(text1)
-        Text(text2)
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "Contact profile picture",
+        )
+        Column()
+        {
+            Text(text1)
+            Text(text2)
+        }
     }
 }
 
