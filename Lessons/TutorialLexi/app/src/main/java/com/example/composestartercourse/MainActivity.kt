c3f91959011c11ff54bd6efaab5c6b4fcc093928
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-02-03 21:15:16 +0000

    Spacer added with modifier

--- Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
+++ Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,8 +49,11 @@ fun MessageCard(text1:String, text2:String)
         Image(
             painter = painterResource(R.drawable.img),
             contentDescription = "Contact profile picture",
-            modifier = Modifier.size(40.dp)
+            modifier = Modifier
+                            .size(40.dp)
+                            .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column()
         {
             Text(text1)
