ca5b8ce0cf315a3768fe90175d01409ed549083e
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-02-03 21:12:17 +0000

    Adjusted size of image using modifier.

--- Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
+++ Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composestartercourse.ui.theme.ComposeStarterCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +44,7 @@ fun MessageCard(text1:String, text2:String)
         Image(
             painter = painterResource(R.drawable.img),
             contentDescription = "Contact profile picture",
+            modifier = Modifier.size(40.dp)
         )
         Column()
         {
