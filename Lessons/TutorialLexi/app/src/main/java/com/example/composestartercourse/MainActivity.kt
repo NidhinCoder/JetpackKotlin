81ad53aa1d34517d39e97d623758103ab3d050f3
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-02-04 04:13:51 +0000

    Material theme added with Surfaace

--- Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
+++ Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.composestartercourse
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -30,7 +34,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(MyMessage("Tom","Jerry"))
+            ComposeStarterCourseTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(MyMessage("Tom", "Jerry"))
+                }
+            }
         }
     }
 }
@@ -43,6 +51,9 @@ fun MessageCard(text: String)
 
 
 //Layouts
+/*
+Material Design is built around three pillars: Color, Typography, and Shape.
+* */
 @Composable
 fun MessageCard(msg: MyMessage)
 {
@@ -54,22 +65,44 @@ fun MessageCard(msg: MyMessage)
             modifier = Modifier
                             .size(40.dp)
                             .clip(CircleShape)
+                            .border(1.5.dp,MaterialTheme.colorScheme.primary)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column()
         {
-            Text(msg.author)
+            Text(
+                text  = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
 
-
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Preview
 @Composable
 fun PreviewMessageCard()
 {
-    MessageCard(MyMessage("Tom","Jerry"))
+    ComposeStarterCourseTheme {
+        Surface {
+            MessageCard(MyMessage("Tom", "Jerry"))
+        }
+    }
 }
