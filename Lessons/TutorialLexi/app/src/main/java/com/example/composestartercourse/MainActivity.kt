9da10c4603c9c69044920087fef35148897cd20c
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-02-04 05:06:32 +0000

    animateContent size() and animateColor as state

--- Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
+++ Lessons/TutorialLexi/app/src/main/java/com/example/composestartercourse/MainActivity.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -31,6 +34,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composestartercourse.ui.theme.ComposeStarterCourseTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 data class MyMessage(val author:String, val body:String)
@@ -54,7 +61,6 @@ fun MessageCard(text: String)
     Text("Hello$text")
 }
 
-
 //Layouts
 /*
 Material Design is built around three pillars: Color, Typography, and Shape.
@@ -73,8 +79,17 @@ fun MessageCard(msg: MyMessage)
                 .border(1.5.dp, MaterialTheme.colorScheme.primary)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column()
-        {
+
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = "",
+        )
+
+        //Make this column as clickable
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){
+
             Text(
                 text  = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -83,10 +98,17 @@ fun MessageCard(msg: MyMessage)
             )
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 0.4.dp,
+                    color = surfaceColor,
+                    //use the animateContentSize modifier to animate the
+                    // message container size smoothly:
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+                    ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
@@ -98,6 +120,11 @@ fun MessageCard(msg: MyMessage)
 @Composable
 fun Conversation(messages:List<MyMessage>)
 {
+    //Lazy column expects a items list, it will internally parse the list,
+    //message is a lambda that will iterate on each items in the list and that
+    //item can be processed as per business logic. Here it is used for MessageCard
+    //It will be worked as a loop..
+
     LazyColumn{
         items(messages){message ->
             MessageCard(message)
