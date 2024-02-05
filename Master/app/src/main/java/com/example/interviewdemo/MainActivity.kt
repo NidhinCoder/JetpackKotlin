8e049f2954d3b449eb24a8d603d4d25759d64985
Mishra <BMISHRA5-adm@jaguarlandrover.com>
2024-02-05 14:58:59 +0000

    Pager concept added

--- Master/app/src/main/java/com/example/interviewdemo/MainActivity.kt
+++ Master/app/src/main/java/com/example/interviewdemo/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.interviewdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.interviewdemo.uilayer.theme.InterviewDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,10 +35,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    HorizontalPagerScreen()
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalPagerScreen() {
+    val pagerState = rememberPagerState(pageCount = { 10 })
+    HorizontalPager(state = pagerState) { page ->
+        ViewPagerItem(page = page)
+    }
+}
+
+@Composable
+fun ViewPagerItem(page: Int, preferredBackground: Color? = null) {
+    val backgroundColor = preferredBackground ?: getBackgroundColorForItem(page)
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Page: $page")
+    }
+}
+
+fun getBackgroundColorForItem(page: Int): Color {
+    return when (page % 2 == 0) {
+        true -> Color.LightGray
+        false -> Color.Gray
     }
 }
 
