2924a0824cfc9628e43df6be1f48c07ffee0aefd
nkuttikk <nkuttikk@jaguarlandrover.com>
2024-12-04 12:58:04 +0000

    NidhinNewsApp added

---
+++ NidhinNews/app/src/main/java/com/jlrinterview/nidhinnews/NewsNavigation.kt
@@ -0,0 +1,12 @@
+package com.jlrinterview.nidhinnews
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+class NewsNavigation {
+}
+
+@Composable
+fun NewsNavigationHost(){
+    val navController  = rememberNavController()
+}
